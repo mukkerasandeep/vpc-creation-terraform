f9facb69dd6ca465cd98acdd469caa5665dae265
sandeep <sandeep@gmail.com>
2022-09-10 14:21:29 +0000

    2

--- provide.tf
+++ provide.tf
@@ -2,15 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.29.0"
+      version = "4.0"
     }
   }
 }
 
 provider "aws" {
   # Configuration options
-  region     = "us-east-1"
-  profile    = "default"
-  access_key = "AKIATZ6OJOW5FLTLKL6M"
-  secret_key = "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"
+  region = "us-east-1"
 }
