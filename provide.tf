f81a3a0a64fc599ac638318540e3260e1abf6744
sandeep <sandeep@gmail.com>
2022-09-10 14:16:27 +0000

    1

--- provide.tf
+++ provide.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "aws" {
   # Configuration options
-  region  = "us-east-1"
-  profile = "default"
+  region     = "us-east-1"
+  profile    = "default"
+  access_key = "AKIATZ6OJOW5FLTLKL6M"
+  secret_key = "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"
 }
